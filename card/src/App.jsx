b17fea11ea0d8bf785cb054c3403ab1dd07e6f34
karan_kacha <85750282+kachakaran6@users.noreply.github.com>
2025-05-17 07:04:32 +0530

    add-to-cart added

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -58,6 +58,11 @@ const ProjectLinks = () => {
       description: "A Coffe website",
       link: "https://cafe-chronicles.netlify.app/",
     },
+        {
+      title: "Add To Cart",
+      description: "A mini project showcasing the functionality of adding items to cart",
+      link: "https://add-to-cart-dun.vercel.app/",
+    },
     {
       title: "To Do Vault",
       description: "For keeping track of what matters",
